16e34df07c23509b831d62299c4e5139f7554821
Shadi187 <101983035+Shadi187@users.noreply.github.com>
2024-01-23 22:18:38 +0200

    Initial Commit

---
+++ Drugly/DruglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DruglyApp.swift
+//  Drugly
+//
+//  Created by boshFbt on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DruglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
